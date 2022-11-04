f90e0cb0a332ea1e73865a90356ef306fdadeca2
shraddha38 <shraddhasharmapro@gmail.com>
2022-11-03 19:53:53 -0500

    feat: firebase setup

---
+++ src/lib/firebase.ts
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCyrPKsktJjTILHYFiKct_HWTRWGloDzAs",
+  authDomain: "hotel-admin-fc2ab.firebaseapp.com",
+  projectId: "hotel-admin-fc2ab",
+  storageBucket: "hotel-admin-fc2ab.appspot.com",
+  messagingSenderId: "826051025813",
+  appId: "1:826051025813:web:1bd7af75359d003ed5349c",
+  measurementId: "G-N45HEZCT1R"
+};
+
+export const app = initializeApp(firebaseConfig);
