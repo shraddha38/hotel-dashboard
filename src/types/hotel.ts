5ce431c9649c92ef25dcbd4a0010f9f7c6869c81
shraddha38 <shraddhasharmapro@gmail.com>
2022-11-03 19:56:09 -0500

    feat: types defined for hotel creation

---
+++ src/types/hotel.ts
@@ -0,0 +1,12 @@
+export interface NewHotelType {
+    country?: string;
+    description?: string;
+    feature?: string;
+    id?: string;
+    imageUrl?: string;
+    includingFoodPrice?: string;
+    noOfReviews?: number;
+    pricePerNight?: string;
+    starsRating?: number;
+    title?: string;
+}
