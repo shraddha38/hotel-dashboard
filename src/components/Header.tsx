53f5e8bd3c5e2700f5ed359a770e123c02be874a
shraddha38 <shraddhasharmapro@gmail.com>
2022-11-03 19:55:38 -0500

    feat; header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,18 +5,36 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
     return (
-        <Box sx={{ flexGrow: 1 }}>
+        <Box sx={{ flexGrow: 1, display: "flex", justifyContent: "space-between" }}>
             <AppBar position="static">
                 <Toolbar variant="dense">
-                    <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6" color="inherit" component="div">
-                      Hotel App
-                    </Typography>
+                    <Box sx={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                        m: 3,
+                    }}>
+                        <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography variant="h6" color="inherit" component="div"> Hotel Findings</Typography>
+
+                    </Box>
+
+
+                    <Box sx={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                        m: 3,
+                        backgroundColor: "lightgray"
+                    }}>
+                        <Link to="/" > All Hotels </Link>
+                        <Link to="/create" > New Hotels </Link>
+                    </Box>
+
                 </Toolbar>
             </AppBar>
         </Box>
